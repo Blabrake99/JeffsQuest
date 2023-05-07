82dd52cf21c28b489b759cea8914572701570cd9
Brandon <Blabrake1@gmail.com>
2023-05-06 20:59:43 -0400

    New movement and camera

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Camera))]
+public class CameraScript : MonoBehaviour
+{
+    PlayerAction actions;
+    [SerializeField, Tooltip("The object the camera is focusing on")] private Transform focus;
+    [SerializeField, Range(1f, 20f), Tooltip("how far the camera's away from the object")] float distance = 5f;
+    [SerializeField, Tooltip("The zoom speed of the camera")] private float zoomSpeed = 10f;
+    [SerializeField, Min(0f)] float focusRadius = 1f;
+    [SerializeField, Range(0f, 1f)] float focusCentering = 0.5f;
+    [SerializeField, Range(1f, 360f), Tooltip("The speed at which the camera can rotate")] float rotationSpeed = 90f;
+    [SerializeField, Range(-89f, 89f), Tooltip("How far the camera can go up and down")] float minVerticalAngle = -30f, maxVerticalAngle = 60f;
+    [SerializeField, Min(0f)] float alignDelay = 5f;
+    [SerializeField, Range(0f, 90f)] float alignSmoothRange = 45f;
+    [SerializeField, Tooltip("The layers that collide with the camera")] LayerMask obstructionMask = -1;
+    Camera regularCamera;
+    Vector2 orbitAngles = new Vector2(45f, 0f);
+    Vector3 focusPoint, previousFocusPoint;
+    float lastManualRotationTime;
+    Quaternion orbitRotation;
+    Quaternion gravityAlignment = Quaternion.identity;
+    void OnValidate()
+    {
+        if (maxVerticalAngle < minVerticalAngle)
+        {
+            maxVerticalAngle = minVerticalAngle;
+        }
+    }
+    private void Start()
+    {
+        GameManager.lockCursor();
+        actions = new PlayerAction();
+        regularCamera = GetComponent<Camera>();
+        actions.Player.Enable();
+        focusPoint = focus.position;
+        transform.localRotation = orbitRotation = Quaternion.Euler(orbitAngles);
+    }
+    void LateUpdate()
+    {
+        gravityAlignment = Quaternion.FromToRotation(gravityAlignment * Vector3.up,
+            CustomGravity.GetUpAxis(focusPoint)) * gravityAlignment;
+        UpdateFocusPoint();
+        if (ManualRotation() || AutomaticRotation())
+        {
+            ConstrainAngles();
+            orbitRotation = Quaternion.Euler(orbitAngles);
+        }
+        Quaternion lookRotation = gravityAlignment * orbitRotation;
+        Vector3 lookDirection = lookRotation * Vector3.forward;
+        Vector3 lookPosition = focusPoint - lookDirection * distance;
+        Vector3 rectOffset = lookDirection * regularCamera.nearClipPlane;
+        Vector3 rectPosition = lookPosition + rectOffset;
+        Vector3 castFrom = focus.position;
+        Vector3 castLine = rectPosition - castFrom;
+        float castDistance = castLine.magnitude;
+        Vector3 castDirection = castLine / castDistance;
+        if (Physics.BoxCast(castFrom, CameraHalfExtends, castDirection, out RaycastHit hit,
+            lookRotation, castDistance, obstructionMask, QueryTriggerInteraction.Ignore))
+        {
+            rectPosition = castFrom + castDirection * hit.distance;
+            lookPosition = rectPosition - rectOffset;
+        }
+        transform.SetPositionAndRotation(lookPosition, lookRotation);
+    }
+    bool ManualRotation()
+    {
+        Vector2 input = new Vector2(actions.Player.Camera.ReadValue<Vector2>().y,
+            actions.Player.Camera.ReadValue<Vector2>().x);
+        const float e = 0.001f;
+        if (input.x < -e || input.x > e || input.y < -e || input.y > e)
+        {
+            orbitAngles += rotationSpeed * Time.unscaledDeltaTime * input;
+            lastManualRotationTime = Time.unscaledTime;
+            return true;
+        }
+        return false;
+    }
+    Vector3 CameraHalfExtends
+    {
+        get
+        {
+            Vector3 halfExtends;
+            halfExtends.y =
+                regularCamera.nearClipPlane *
+                Mathf.Tan(0.5f * Mathf.Deg2Rad * regularCamera.fieldOfView);
+            halfExtends.x = halfExtends.y * regularCamera.aspect;
+            halfExtends.z = 0f;
+            return halfExtends;
+        }
+    }
+    bool AutomaticRotation()
+    {
+        if (Time.unscaledTime - lastManualRotationTime < alignDelay)
+        {
+            return false;
+        }
+        Vector3 alignedDelta = Quaternion.Inverse(gravityAlignment) * (focusPoint - previousFocusPoint);
+        Vector2 movement = new Vector2(alignedDelta.x, alignedDelta.z);
+        float movementDeltaSqr = movement.sqrMagnitude;
+        if (movementDeltaSqr < 0.0001f)
+        {
+            return false;
+        }
+        float headingAngle = GetAngle(movement / Mathf.Sqrt(movementDeltaSqr));
+        float deltaAbs = Mathf.Abs(Mathf.DeltaAngle(orbitAngles.y, headingAngle));
+        float rotationChange = rotationSpeed * Mathf.Min(Time.unscaledDeltaTime, movementDeltaSqr);
+        if (deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= deltaAbs / alignSmoothRange;
+        }
+        else if (180f - deltaAbs < alignSmoothRange)
+        {
+            rotationChange *= (180f - deltaAbs) / alignSmoothRange;
+        }
+        orbitAngles.y = Mathf.MoveTowardsAngle(orbitAngles.y, headingAngle, rotationChange);
+        return true;
+    }
+    static float GetAngle(Vector2 direction)
+    {
+        float angle = Mathf.Acos(direction.y) * Mathf.Rad2Deg;
+        return direction.x < 0f ? 360f - angle : angle;
+    }
+    void ConstrainAngles()
+    {
+        orbitAngles.x =
+            Mathf.Clamp(orbitAngles.x, minVerticalAngle, maxVerticalAngle);
+
+        if (orbitAngles.y < 0f)
+        {
+            orbitAngles.y += 360f;
+        }
+        else if (orbitAngles.y >= 360f)
+        {
+            orbitAngles.y -= 360f;
+        }
+    }
+    void UpdateFocusPoint()
+    {
+        previousFocusPoint = focusPoint;
+        Vector3 targetPoint = focus.position;
+        if (focusRadius > 0f)
+        {
+            float distance = Vector3.Distance(targetPoint, focusPoint);
+            float t = 1f;
+            if (distance > 0.01f && focusCentering > 0f)
+            {
+                t = Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime);
+            }
+            if (distance > focusRadius)
+            {
+                t = Mathf.Min(t, focusRadius / distance);
+            }
+            focusPoint = Vector3.Lerp(targetPoint, focusPoint, t);
+        }
+        else
+        {
+            focusPoint = targetPoint;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,448 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMovement : MonoBehaviour
+{
+    protected PlayerAction actions;
+    [SerializeField, Range(0f, 100f), Tooltip("Max speed after Acceleration")] float maxSpeed = 10f, maxClimbSpeed = 2f, maxSwimSpeed = 5f;
+    [SerializeField, Range(0f, 100f), Tooltip("Acceleration for their respective names")] float maxAcceleration = 10f, maxAirAcceleration = 1f, maxSwimAcceleration = 5f, maxClimbAcceleration = 20f;
+    [SerializeField, Range(0f, 10f), Tooltip("How high the player jumps")] float jumpHeight = 2f;
+    [SerializeField, Range(0, 5), Tooltip("How many jumps the players allowed to do")] int maxAirJumps = 1;
+    [SerializeField, Range(0f, 90f), Tooltip("This is the max ground angle to tell if the players Grounded or not")]
+    float maxGroundAngle = 25f, maxStairsAngle = 50f;
+    [SerializeField, Range(0f, 100f)] float maxSnapSpeed = 100f;
+    [SerializeField, Min(0f), Tooltip("This is a raycast distance for below the player")] float probeDistance = 1f;
+    [SerializeField, Tooltip("The layermasks for their respective names")] LayerMask probeMask = -1, stairsMask = -1, waterMask = 0, climbMask = -1;
+    [SerializeField, Tooltip("The Transform of the camera")] Transform playerInputSpace = default;
+    [SerializeField, Tooltip("THe offset of water o nthe player. the higher the number the lower jeff will be in the water" +
+        "befor he's at the top")]
+    float submergenceOffset = 0.5f;
+    [SerializeField, Range(90f, 170f)] private float maxClimbingAngle = 140f;
+    [SerializeField, Min(0.1f), Tooltip("This is for detecting when the player is fully submerged")] float submergenceRange = 1f;
+    [SerializeField, Range(0f, 10f), Tooltip("Makes movement more sluggesh underwater")] float waterDrag = 1f;
+    [SerializeField, Min(0f), Tooltip("the lower the bouyancy the faster it sinks underwater")] float buoyancy = 1f;
+    [SerializeField, Range(0.01f, 1f), Tooltip("This defines the minimum submergence required for swimming")] float swimThreshold = 0.5f;
+    int jumpPhase;
+    private Rigidbody _body, _connectedBody, _previousConnectedBody;
+    private Vector3 _playerInput;
+    private Vector3 _velocity, _connectionVelocity;
+    private Vector3 _connectionWorldPosition, _connectionLocalPosition;
+    private Vector3 _contactNormal, _steepNormal, _climbNormal, _lastClimbNormal;
+    private Vector3 _upAxis, _rightAxis, _forwardAxis;
+    private bool _desiresClimbing;
+    private float _desiredJump;
+    private int _groundContactCount, _steepContactCount, _climbContactCount;
+    private bool ONGround => _groundContactCount > 0;
+    private bool ONSteep => _steepContactCount > 0;
+    private bool Climbing => _climbContactCount > 0 && _stepsSinceLastJump > 2;
+    private int _jumpPhase;
+    private float _minGroundDotProduct, _minStairsDotProduct, _minClimbDotProduct;
+    private int _stepsSinceLastGrounded, _stepsSinceLastJump;
+    private bool InWater => _submergence > 0f;
+    private float _submergence;
+    private bool Swimming => _submergence >= swimThreshold;
+    private void OnValidate()
+    {
+        _minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+        _minStairsDotProduct = Mathf.Cos(maxStairsAngle * Mathf.Deg2Rad);
+        _minClimbDotProduct = Mathf.Cos(maxClimbingAngle * Mathf.Deg2Rad);
+    }
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody>();
+        _body.useGravity = false;
+        OnValidate();
+    }
+    void Start()
+    {
+        OnValidate();
+        actions = new PlayerAction();
+        actions.Player.Enable();
+    }
+    void Update()
+    {
+        _playerInput = actions.Player.Move.ReadValue<Vector2>();
+        //playerInput.z = Swimming ? Input.GetAxis("UpDown") : 0f;
+        _playerInput = Vector3.ClampMagnitude(_playerInput, 1f);
+        _desiredJump = actions.Player.Jump.ReadValue<float>();
+        if (playerInputSpace)
+        {
+            _rightAxis = ProjectDirectionOnPlane(playerInputSpace.right, _upAxis);
+            _forwardAxis =
+                ProjectDirectionOnPlane(playerInputSpace.forward, _upAxis);
+        }
+        else
+        {
+            _rightAxis = ProjectDirectionOnPlane(Vector3.right, _upAxis);
+            _forwardAxis = ProjectDirectionOnPlane(Vector3.forward, _upAxis);
+        }
+
+        if (Swimming)
+        {
+            _desiresClimbing = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        var gravity = CustomGravity.GetGravity(_body.position, out _upAxis);
+        UpdateState();
+
+        if (InWater)
+        {
+            _velocity *= 1f - waterDrag * _submergence * Time.deltaTime;
+        }
+
+        AdjustVelocity();
+
+        if (_desiredJump > 0.05)
+        {
+            _desiredJump = 0;
+            Jump(gravity);
+        }
+        if (Climbing)
+        {
+            _velocity -= _contactNormal * (maxClimbAcceleration * 0.9f * Time.deltaTime);
+        }
+        else if (InWater)
+        {
+            _velocity += gravity * ((1f - buoyancy * _submergence) * Time.deltaTime);
+        }
+        else if (ONGround && _velocity.sqrMagnitude < 0.01f)
+        {
+            _velocity +=
+                _contactNormal *
+                (Vector3.Dot(gravity, _contactNormal) * Time.deltaTime);
+        }
+        else if (_desiresClimbing && ONGround)
+        {
+            _velocity += (gravity - _contactNormal * (maxClimbAcceleration * 0.9f)) * Time.deltaTime;
+        }
+        else
+        {
+            _velocity += gravity * Time.deltaTime;
+        }
+
+        _body.velocity = _velocity;
+        print(_body.velocity.y);
+        ClearState();
+    }
+    private void ClearState()
+    {
+        _groundContactCount = _steepContactCount = _climbContactCount = 0;
+        _contactNormal = _steepNormal = _climbNormal = Vector3.zero;
+        _connectionVelocity = Vector3.zero;
+        _previousConnectedBody = _connectedBody;
+        _connectedBody = null;
+        _submergence = 0f;
+    }
+    private void Jump(Vector3 gravity)
+    {
+        Vector3 jumpDirection;
+        if (ONGround)
+        {
+            jumpDirection = _contactNormal;
+        }
+        else if (ONSteep)
+        {
+            jumpDirection = _steepNormal;
+            _jumpPhase = 0;
+        }
+        else if (maxAirJumps > 0 && _jumpPhase <= maxAirJumps)
+        {
+            if (_jumpPhase == 0)
+            {
+                _jumpPhase = 1;
+            }
+
+            jumpDirection = _contactNormal;
+        }
+        else
+        {
+            return;
+        }
+
+        _stepsSinceLastJump = 0;
+        _jumpPhase += 1;
+        var jumpSpeed = Mathf.Sqrt(2f * gravity.magnitude * jumpHeight);
+        if (InWater)
+        {
+            jumpSpeed *= Mathf.Max(0f, 1f - _submergence / swimThreshold);
+        }
+        jumpDirection = (jumpDirection + _upAxis).normalized;
+        var alignedSpeed = Vector3.Dot(_velocity, jumpDirection);
+        if (alignedSpeed > 0f)
+        {
+            jumpSpeed = Mathf.Max(jumpSpeed - _velocity.y, 0f);
+        }
+
+        _velocity += jumpDirection * jumpSpeed;
+    }
+    Vector3 ProjectDirectionOnPlane(Vector3 direction, Vector3 normal)
+    {
+        return (direction - normal * Vector3.Dot(direction, normal)).normalized;
+    }
+    private void AdjustVelocity()
+    {
+        float acceleration, speed;
+        Vector3 xAxis, zAxis;
+        if (Climbing)
+        {
+            acceleration = maxClimbAcceleration;
+            speed = maxClimbSpeed;
+            xAxis = Vector3.Cross(_contactNormal, _upAxis);
+            zAxis = _upAxis;
+        }
+        else if (InWater)
+        {
+            var swimFactor = Mathf.Min(1f, _submergence / swimThreshold);
+            acceleration = Mathf.LerpUnclamped(ONGround ? maxAcceleration : maxAirAcceleration, maxSwimAcceleration, swimFactor);
+            speed = Mathf.LerpUnclamped(maxSpeed, maxSwimSpeed, swimFactor);
+            xAxis = _rightAxis;
+            zAxis = _forwardAxis;
+        }
+        else
+        {
+            acceleration = ONGround ? maxAcceleration : maxAirAcceleration;
+            speed = ONGround && _desiresClimbing ? maxClimbSpeed : maxSpeed;
+            xAxis = _rightAxis;
+            zAxis = _forwardAxis;
+        }
+
+        xAxis = ProjectDirectionOnPlane(xAxis, _contactNormal);
+        zAxis = ProjectDirectionOnPlane(zAxis, _contactNormal);
+
+        var relativeVelocity = _velocity - _connectionVelocity;
+        var currentX = Vector3.Dot(relativeVelocity, xAxis);
+        var currentZ = Vector3.Dot(relativeVelocity, zAxis);
+
+        var maxSpeedChange = acceleration * Time.deltaTime;
+
+        var newX = Mathf.MoveTowards(currentX, _playerInput.x * speed, maxSpeedChange);
+        var newZ = Mathf.MoveTowards(currentZ, _playerInput.y * speed, maxSpeedChange);
+
+        _velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
+
+        if (Swimming)
+        {
+            var currentY = Vector3.Dot(relativeVelocity, _upAxis);
+            var newY = Mathf.MoveTowards(currentY, _playerInput.z * speed, maxSpeedChange);
+            _velocity += _upAxis * (newY - currentY);
+        }
+    }
+    private void UpdateState()
+    {
+        _stepsSinceLastGrounded += 1;
+        _stepsSinceLastJump += 1;
+        _velocity = _body.velocity;
+        if (CheckClimbing() || CheckSwimming() || ONGround || SnapToGround() || CheckSteepContacts())
+        {
+            _stepsSinceLastGrounded = 0;
+
+            if (_stepsSinceLastJump > 1)
+            {
+                _jumpPhase = 0;
+            }
+
+            if (_groundContactCount > 1)
+            {
+                _contactNormal.Normalize();
+            }
+        }
+        else
+        {
+            _contactNormal = _upAxis;
+        }
+
+        if (_connectedBody)
+        {
+            if (_connectedBody.isKinematic || _connectedBody.mass >= _body.mass)
+            {
+                UpdateConnectionState();
+            }
+        }
+    }
+    private bool CheckClimbing()
+    {
+        if (Climbing)
+        {
+            if (_climbContactCount > 1)
+            {
+                _climbNormal.Normalize();
+                var upDot = Vector3.Dot(_upAxis, _climbNormal);
+                if (upDot >= _minGroundDotProduct)
+                {
+                    _climbNormal = _lastClimbNormal;
+                }
+            }
+            _groundContactCount = 1;
+            _contactNormal = _climbNormal;
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool CheckSwimming()
+    {
+        if (Swimming)
+        {
+            _groundContactCount = 0;
+            _contactNormal = _upAxis;
+            return true;
+        }
+        return false;
+    }
+    private void UpdateConnectionState()
+    {
+        if (_connectedBody == _previousConnectedBody)
+        {
+            var connectionMovement = _connectedBody.transform.TransformPoint(_connectionLocalPosition) -
+                                     _connectionWorldPosition;
+            _connectionVelocity = connectionMovement / Time.deltaTime;
+        }
+
+        _connectionWorldPosition = _body.position;
+        _connectionLocalPosition = _connectedBody.transform.InverseTransformPoint(_connectionWorldPosition);
+    }
+    private bool SnapToGround()
+    {
+        if (_stepsSinceLastGrounded > 1 || _stepsSinceLastJump <= 2)
+        {
+            return false;
+        }
+
+        var speed = _velocity.magnitude;
+        if (speed > maxSnapSpeed)
+        {
+            return false;
+        }
+
+        if (!Physics.Raycast(_body.position, -_upAxis, out var hit, probeDistance, probeMask, QueryTriggerInteraction.Ignore))
+        {
+            return false;
+        }
+
+        var upDot = Vector3.Dot(_upAxis, hit.normal);
+        if (upDot < GetMinDot(hit.collider.gameObject.layer))
+        {
+            return false;
+        }
+
+        _groundContactCount = 1;
+        _contactNormal = hit.normal;
+        var dot = Vector3.Dot(_velocity, hit.normal);
+        if (dot > 0f)
+        {
+            _velocity = (_velocity - hit.normal * dot).normalized * speed;
+        }
+
+        _connectedBody = hit.rigidbody;
+        return true;
+    }
+    private float GetMinDot(int layer)
+    {
+        return (stairsMask & (1 << layer)) == 0 ? _minGroundDotProduct : _minStairsDotProduct;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if ((waterMask & (1 << col.gameObject.layer)) != 0)
+        {
+            EvaluateSubmergence(col);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if ((waterMask & (1 << col.gameObject.layer)) != 0)
+        {
+            EvaluateSubmergence(col);
+        }
+    }
+    private void EvaluateSubmergence(Collider collision)
+    {
+        if (Physics.Raycast(_body.position + _upAxis * submergenceOffset, -_upAxis, out RaycastHit hit,
+            submergenceRange + 1f, waterMask, QueryTriggerInteraction.Collide))
+        {
+            _submergence = 1f - hit.distance / submergenceRange;
+        }
+        else
+        {
+            _submergence = 1f;
+        }
+        if (Swimming)
+        {
+            _connectedBody = collision.attachedRigidbody;
+        }
+    }
+    private void EvaluateCollision(Collision collision)
+    {
+        if (Swimming)
+        {
+            return;
+        }
+        var layer = collision.gameObject.layer;
+        var minDot = GetMinDot(layer);
+        for (var i = 0; i < collision.contactCount; i++)
+        {
+            var normal = collision.GetContact(i).normal;
+            var upDot = Vector3.Dot(_upAxis, normal);
+
+            if (upDot >= minDot)
+            {
+                _groundContactCount += 1;
+                _contactNormal += normal;
+                _connectedBody = collision.rigidbody;
+            }
+            else
+            {
+                if (upDot > -0.01f)
+                {
+                    _steepContactCount += 1;
+                    _steepNormal += normal;
+                    if (_groundContactCount == 0)
+                    {
+                        _connectedBody = collision.rigidbody;
+                    }
+                }
+
+                if (_desiresClimbing && upDot >= _minClimbDotProduct && (climbMask & (1 << layer)) != 0)
+                {
+                    _climbContactCount += 1;
+                    _climbNormal += normal;
+                    _lastClimbNormal = normal;
+                    _connectedBody = collision.rigidbody;
+                }
+            }
+        }
+    }
+    private bool CheckSteepContacts()
+    {
+        if (_steepContactCount > 1)
+        {
+            _steepNormal.Normalize();
+            var upDot = Vector3.Dot(_upAxis, _steepNormal);
+            if (upDot >= _minGroundDotProduct)
+            {
+                _steepContactCount = 0;
+                _groundContactCount = 1;
+                _contactNormal = _steepNormal;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+}
