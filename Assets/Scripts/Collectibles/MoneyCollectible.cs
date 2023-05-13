1753050c7e7f287dd770fc358fb0503dc29e693c
Brandon <Blabrake1@gmail.com>
2023-05-12 21:08:31 -0400

    fixed coin rotation

--- Assets/Scripts/Collectibles/MoneyCollectible.cs
+++ Assets/Scripts/Collectibles/MoneyCollectible.cs
@@ -4,10 +4,10 @@ public class MoneyCollectible : MonoBehaviour, ICollectibles
 {
     public int currencyGained = 1;
     [SerializeField, Range(0, 100)] float RotationSpeed = 45;
-    private void Update()
-    {
-        transform.Rotate(-Vector3.right * RotationSpeed * Time.deltaTime);
-    }
+    //private void Update()
+    //{
+    //    transform.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
+    //}
     public void Collected()
     {
         GameManager.GainMoney(currencyGained);
