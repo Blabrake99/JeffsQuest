bb2c34d86beeb3454033c8d0436394bccae3ee54
Brandon LaBrake <56169332+Blabrake99@users.noreply.github.com>
2024-11-24 23:03:58 -0500

    BreakingPlatform

---
+++ Assets/Scripts/PlatformScripts/BreakOnStepPlatform.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakOnStepPlatform : MonoBehaviour, IPlatforms
+{
+    [SerializeField] float turnOffTime = 3;
+    [SerializeField] bool breakRegaurdlessOfPlayer = true;
+    [SerializeField] float StayTurnedOffFor = 3;
+    bool active;
+    float timer;
+    [SerializeField] Collider collider;
+    bool turnedOff;
+    public void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void DeActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+    void Update()
+    {
+        if(active)
+        {
+            timer -= Time.deltaTime;
+            if(timer < 0 && !turnedOff)
+            {
+                collider.enabled = false;
+                timer = StayTurnedOffFor;
+                turnedOff = true;
+            }
+            if (timer < 0 && turnedOff)
+            {
+                collider.enabled = true;
+                timer = StayTurnedOffFor;
+                turnedOff = false;
+                active = false;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider col)
+    {
+        Player player = col.GetComponent<Player>();
+        if (player != null)
+        {
+            timer = turnOffTime;
+            active = true;
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        Player player = col.GetComponent<Player>();
+        if (player != null && !breakRegaurdlessOfPlayer)
+        {
+            active = false;
+        }
+    }
+}
